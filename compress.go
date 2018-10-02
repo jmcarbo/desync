7e358c188bf9ce066e55685ebe6556f7665977cc
Joan Marc Carbo <jmcarbo@gmail.com>
2018-10-02 06:44:50 +0200

    Got rid of external compression library

--- compress.go
+++ compress.go
@@ -1,17 +1,19 @@
 package desync
 
 import (
-	"github.com/datadog/zstd"
+//	"github.com/datadog/zstd"
 )
 
 // Compress a block using the only (currently) supported algorithm
 func Compress(b []byte) ([]byte, error) {
-	return zstd.CompressLevel(nil, b, 3)
+	//return zstd.CompressLevel(nil, b, 3)
+  return b, nil
 }
 
 // Decompress a block using the only supported algorithm. If you already have
 // a buffer it can be passed into out and will be used. If out=nil, a buffer
 // will be allocated.
 func Decompress(out, in []byte) ([]byte, error) {
-	return zstd.Decompress(out, in)
+        copy(in, out)
+	return out, nil
 }
